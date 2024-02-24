b05017e2a7a8521ac0aad37c681cb864c862fee5
Stefan Seeland <168659+stesee@users.noreply.github.com>
2024-02-24 21:28:56 +0100

    Create MacOsSpecificIssue.cs

---
+++ SanitizeFilenameTests/MacOsSpecificIssue.cs
@@ -0,0 +1,23 @@
+﻿using Codeuctivity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SanitizeFilenameTests
+{
+    internal class MacOsSpecificIssue
+    {
+        [Test]
+        public void ReproducesIssueThatOnlyOccuresRunningOnMacos()
+        {
+            // All values listed in InvalidCharsInMacOsFileNames trigger an exception when trying to create a file with that name on MacOS
+            // This test is a minimal example to reproduce the issue using the character '줰' (U+C930) which trigger an exception in MacOS
+
+            var filenameThatCantBeWrittonOnMacOs = "filename줰.txt";
+            File.WriteAllText(filenameThatCantBeWrittonOnMacOs, "content");
+            Assert.Pass("This test will only fail on MacOS");
+        }
+    }
+}
