340794233c2d1ef9fcb7b9507bfb8943dc065d23
Stefan Seeland <168659+stesee@users.noreply.github.com>
2024-02-24 21:39:22 +0100

    Update MacOsSpecificIssue.cs

--- SanitizeFilenameTests/MacOsSpecificIssue.cs
+++ SanitizeFilenameTests/MacOsSpecificIssue.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using static System.Runtime.InteropServices.JavaScript.JSType;
 
 namespace SanitizeFilenameTests
 {
@@ -17,6 +18,11 @@ namespace SanitizeFilenameTests
 
             var filenameThatCantBeWrittonOnMacOs = "filename줰.txt";
             File.WriteAllText(filenameThatCantBeWrittonOnMacOs, "content");
+
+            char aValidChar = (char)930;
+            var valid = "valid" + new string(aValidChar, 1) + "filename"
+            File.WriteAllText(filenameThatCantBeWrittonOnMacOs, "content");
+
             Assert.Pass("This test will only fail on MacOS");
         }
     }
