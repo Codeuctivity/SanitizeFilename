62759a24a2dce41f88e48218651c5b81a7ce5142
Stefan Seeland <stefan.seeland@gmail.com>
2024-03-02 22:52:31 +0100

    Test github runner for specific behavior

--- SanitizeFilenameTests/LinuxSpecificTests.cs
+++ SanitizeFilenameTests/LinuxSpecificTests.cs
@@ -8,7 +8,15 @@ namespace SanitizeFilenameTests
         [Test]
         public void ShouldFailToWriteFilenameWithMoreThan255Bytes()
         {
-            var fileNameWithUnpairedSurrogate = new string('a', 248) + "👩🏽‍🚒";
+            var fileNameTooLongForLinux = new string('a', 248) + "👩🏽‍🚒";
+            var actual = FileWriteAsserter.TryWriteFileToTempDirectory(fileNameTooLongForLinux);
+            Assert.That(actual, Is.Not.EqualTo(RuntimeInformation.IsOSPlatform(OSPlatform.Linux)));
+        }
+
+        [Test]
+        public void ShouldFailToWriteUnpairedSurrogateOnGithubRunner()
+        {
+            string fileNameWithUnpairedSurrogate = "filename" + (char)55296;
             var actual = FileWriteAsserter.TryWriteFileToTempDirectory(fileNameWithUnpairedSurrogate);
             Assert.That(actual, Is.Not.EqualTo(RuntimeInformation.IsOSPlatform(OSPlatform.Linux)));
         }
