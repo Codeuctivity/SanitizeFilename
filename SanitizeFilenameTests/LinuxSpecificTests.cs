f6b210e6d8da28fd15a5f0d7279f8ac21e2d0032
Stefan Seeland <stefan.seeland@gmail.com>
2024-03-02 21:27:05 +0100

    Add Linux specific tests

---
+++ SanitizeFilenameTests/LinuxSpecificTests.cs
@@ -0,0 +1,16 @@
+﻿using Codeuctivity;
+using System.Runtime.InteropServices;
+
+namespace SanitizeFilenameTests
+{
+    internal class LinuxSpecificTests
+    {
+        [Test]
+        public void ShouldFailToWriteFilenameWithMoreThan255Bytes()
+        {
+            var fileNameWithUnpairedSurrogate = new string('a', 248) + "👩🏽‍🚒";
+            var actual = FileWriteAsserter.TryWriteFileToTempDirectory(fileNameWithUnpairedSurrogate);
+            Assert.That(actual, Is.Not.EqualTo(RuntimeInformation.IsOSPlatform(OSPlatform.Linux)));
+        }
+    }
+}
---
+++ SanitizeFilenameTests/MacOsSpecificTests.cs
@@ -0,0 +1,53 @@
+﻿using Codeuctivity;
+using System.Globalization;
+using System.Runtime.InteropServices;
+
+namespace SanitizeFilenameTests
+{
+    internal class MacOsSpecificTests
+    {
+        [Test]
+        public void ShouldFailToWriteUnpairedSurrogateOnlyOnMacOs()
+        {
+            string unpairedSurrogate = "\uD800";
+            var fileNameWithUnpairedSurrogate = unpairedSurrogate + "fileName.txt";
+            var actual = FileWriteAsserter.TryWriteFileToTempDirectory(fileNameWithUnpairedSurrogate);
+            Assert.That(actual, Is.Not.EqualTo(RuntimeInformation.IsOSPlatform(OSPlatform.OSX)));
+        }
+
+        [Test]
+        public void InvalidCharsInMacOsFilenameShouldFailToWriteFile()
+        {
+            foreach (var invalidOnMacOs in SanitizeFilename.InvalidCharsInMacOsFileNames)
+            {
+                var filenameInvalidOnMacOs = invalidOnMacOs + "Filename.txt";
+                var actual = FileWriteAsserter.TryWriteFileToTempDirectory(filenameInvalidOnMacOs);
+                Assert.That(actual, Is.Not.EqualTo(RuntimeInformation.IsOSPlatform(OSPlatform.OSX)));
+            }
+        }
+
+        [Test]
+        public void ShouldProofThatThereAreOnlyKnownExceptionsInListOfInvalidUnicodeCodePoints()
+        {
+            foreach (var item in SanitizeFilename.InvalidCharsInMacOsFileNames)
+            {
+                var category = CharUnicodeInfo.GetUnicodeCategory(item);
+
+                if (item == (char)3315)
+                {
+                    Assert.That(category, Is.EqualTo(UnicodeCategory.SpacingCombiningMark));
+                }
+                else if (item == (char)3790)
+                {
+                    Assert.That(category, Is.EqualTo(UnicodeCategory.NonSpacingMark));
+                }
+                else
+                {
+                    Assert.That(category, Is.EqualTo(UnicodeCategory.OtherNotAssigned));
+                }
+            }
+        }
+    }
+}
+
+
